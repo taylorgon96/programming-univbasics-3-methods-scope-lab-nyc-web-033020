641f8fe42e01a60f588a92d143d8526c6f6df26c
Taylor Gonzalez <taylorgon@gmail.com>
2020-02-26 23:08:47 +0000

    Automatically backed up by Learn

--- spec/catch_phrases_spec.rb
+++ spec/catch_phrases_spec.rb
@@ -2,7 +2,6 @@ require 'spec_helper'
 
 describe "mario" do
   it "puts out 'It's-a me, Mario!'" do
-    phrase = "It's-a me, Mario!"
     expect{mario}.to output("It's-a me, Mario!\n").to_stdout
   end
 end
